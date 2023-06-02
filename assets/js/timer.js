bcd9e9bb69d1b29081c9f5382e02a608d79f0c38
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2023-06-02 09:20:59 -0700

    Add files via upload

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,25 +1,38 @@
 /**************/
 /* Live Timer */
 /**************/
-var deadline = new Date("JULY 4, 2023 12:00:00").getTime();
+var deadline = localStorage.getItem("deadline");
+var x = null;
 
-var x = setInterval(function() {
+function setTimer() {
+  var inputDeadline = new Date(document.getElementById("deadline").value).getTime();
+  if (isNaN(inputDeadline)) {
+    alert("Invalid date format. Please enter a valid deadline.");
+    return;
+  }
 
-var now = new Date().getTime();
-var t = deadline - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("day").innerHTML =days ;
-document.getElementById("hour").innerHTML =hours;
-document.getElementById("minute").innerHTML = minutes;
-document.getElementById("second").innerHTML =seconds;
-if (t < 0) {
-		clearInterval(x);
-		document.getElementById("demo").innerHTML = "COMPLETE!";
-		document.getElementById("day").innerHTML ='0';
-		document.getElementById("hour").innerHTML ='0';
-		document.getElementById("minute").innerHTML ='0' ;
-		document.getElementById("second").innerHTML = '0'; }
-}, 1000);
+  deadline = inputDeadline;
+  localStorage.setItem("deadline", deadline);
+
+  clearInterval(x);
+  x = setInterval(function() {
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("day").innerHTML = days;
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("minute").innerHTML = minutes;
+    document.getElementById("second").innerHTML = seconds;
+    if (t < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "<a href='https://www.w3schools.com/js/DEFAULT.asp' target='_blank'>COMPLETE</a>";
+      document.getElementById("day").innerHTML = '0';
+      document.getElementById("hour").innerHTML = '0';
+      document.getElementById("minute").innerHTML = '0';
+      document.getElementById("second").innerHTML = '0';
+    }
+  }, 1000);
+}
